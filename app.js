6db1610aff1bf422963ffc42f97a7c9cd1fddf33
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-04-30 16:14:02 -0400

    with AWS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Testing Jenkins CI/CD!')
+  res.send('Testing Jenkins CI/CD with AWS!')
 })
 
 app.listen(3000, function () {
