1f56dd7007cb07dcfac22112b1fde6fba3d4c32c
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-04-30 15:36:55 -0400

    testing jenking pipeline..

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Testing Multi-cloud Orchestration!')
+  res.send('Testing Jenkins Continous Integration!')
 })
 
 app.listen(3000, function () {
