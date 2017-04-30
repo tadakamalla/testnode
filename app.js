16ef4944562a774749c32003608e48d9f2a27bac
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-04-30 16:11:33 -0400

    Testing CI/CD

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Testing Jenkins Continous Integration!')
+  res.send('Testing Jenkins CI/CD!')
 })
 
 app.listen(3000, function () {
