d570f5e3171d45f56a193fa03b3af29117b7d115
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-04-30 14:48:45 -0400

    testing multi cloud orchestration

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello NodeJS!')
+  res.send('Testing Multi-cloud Orchestration!')
 })
 
 app.listen(3000, function () {
