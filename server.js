82f640cca7f9f700bb5f3b71630409fde1ef8206
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-05-03 10:56:14 -0400

    PBI

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 'use strict';
 
 var http = require('http');
-var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8085;
 var app = express();
-var fs = require("fs");
-app.use('/', express.static(__dirname + '/public'));
 
 
 
 app.get('/', function (req, res) {
-	fs.readFile("employee.json", 'utf8', function (err, data) {
-	       console.log( data );
-	       res.send( data );
-	   });
-
+	  res.send('Purple Belt Demo!')
 	})
 
 app.listen(PORT, function () {
