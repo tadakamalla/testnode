c444c5523a4b9f20f9695dd221897d467df34605
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-05-03 10:21:50 -0400

    path

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 'use strict';
 
 var http = require('http');
+var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8085;
 var app = express();
 var fs = require("fs");
-var jsonData = '';
+
 
 
 app.get('/', function (req, res) {
-	fs.readFile('./employee.json', 'utf8', function (err, data) {
-		  if(err) throw err;
-		  jsonData = JSON.parse(data);
-		  res.send(jsonData);
-		});
+	fs.readFile( __dirname + "/" + "employee.json", 'utf8', function (err, data) {
+	       console.log( data );
+	       res.end( data );
+	   });
 
 	})
 
