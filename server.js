2e9d0d9304c95b67dbe6ca37b8ac001eec385972
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-05-03 11:31:14 -0400

    gfg

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8085;
 var app = express();
+var path = require('path');
 
 
 
-app.get('/', function (req, res) {
+app.get('/u01/app/', function (req, res) {
 	  res.send(employee.json);
 	})
 
