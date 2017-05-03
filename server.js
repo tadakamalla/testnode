d9a2d0cf75e52264580df7c7021e1d61569a6fc2
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-05-03 09:45:15 -0400

    testing...

--- server.js
+++ server.js
@@ -10,10 +10,9 @@ var fs = require("fs");
 
 
 app.get('/', function (req, res) {
-
-	   fs.readFile( __dirname + "/" + "employee.json", 'utf8', function (err, data) {
+	fs.readFile( __dirname + "/" + "employee.json", 'utf8', function (err, data) {
 	       console.log( data );
-	       res.end( data );
+	       res.send( data );
 	   });
 
 	})
