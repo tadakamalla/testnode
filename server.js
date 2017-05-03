3f8b699c1d4e8d2f06a9302653a85f0c5c2fa8dd
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-05-03 11:53:39 -0400

    dfd

--- server.js
+++ server.js
@@ -5,18 +5,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8085;
 var app = express();
-var path = require('path');
-var fs = require("fs");
 
 
 
 app.get('/', function (req, res) {
-	 var empFile="/u01/app/employee.json";
-	 fs.readFile(empFile, function (err,data){
-	     response.contentType("application/json");
-	     response.send(data);
-	  });
-	});
+	  res.send('Purple Belt Demo!')
+	})
 
 app.listen(PORT, function () {
     console.log('listening on port ' + PORT)
