4c375a84c14d25b80558358aed50d8623b344ec8
root <root@orchestration.localdomain>
2017-08-25 13:35:07 +0000

    wercker build time!

---
+++ .wercker/builds/a9f3d571-e2c4-44cd-ab94-b907f908c51e/script-2edd417a-e63d-4d28-8da9-038f98b82380/run.sh
@@ -0,0 +1,2 @@
+set -e
+curl -k -X POST https://140.86.35.3/api/v2/deployments/tadakamalla-mynode-20170824-203235/webhook/restart?token=woeqabzg
---
+++ .wercker/builds/c2984514-6a4d-4cf9-88d6-b5f38979add0/script-b9d295c8-b72a-42d7-ba71-44aebf89909b/run.sh
@@ -0,0 +1,2 @@
+set -e
+curl -k -X POST https://140.86.35.3/api/v2/deployments/tadakamalla-mynode-20170824-204108/webhook/restart?token=hssnkcpr
