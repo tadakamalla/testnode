c2acb8ed1b1fb5e89a8fa6081479ad13fbbc5e5c
Aravind Tadakamalla <tadakamalla@gmail.com>
2017-09-05 11:49:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ var PORT = process.env.PORT || 8085;
 var app = express();
 var fs = require("fs");
 var path = require('path');
-var favicon = require('serve-favicon');
 
 
-app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', function (req, res) {
 	fs.readFile( __dirname + "/" + "employee.json", 'utf8', function (err, data) {
